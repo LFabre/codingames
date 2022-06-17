a1d67a3a87af8aae276df67f7f5f2c505e00acad
LFabre <lucas.p.fabre@gmail.com>
2022-06-17 11:08:46 +0100

    Event - Gerrymandering - Typescript

---
+++ CodinGame/Events/Gerrymandering/Gerrymandering.ts
@@ -0,0 +1,35 @@
+const [W, H] = readline().split(' ').map(Number);
+
+const voters: number[][] = [];
+for (let i = 0; i < H; i++) {
+  voters.push(readline().split(' ').map(Number));
+}
+
+function gerry(voters: number[][], w: number, h: number, cache = {}) {
+  const id = `${w}-${h}`;
+  if (cache[id] !== undefined) {
+    return cache[id];
+  }
+
+  let v = voters[h - 1][w - 1];
+
+  // Horizontal Cut
+  for (let x = 1; x < w; x++) {
+    v = Math.max(
+      v,
+      gerry(voters, x, h, cache) + gerry(voters, w - x, h, cache)
+    );
+  }
+
+  // Vertical Cut
+  for (let y = 1; y < h; y++) {
+    v = Math.max(
+      v,
+      gerry(voters, w, y, cache) + gerry(voters, w, h - y, cache)
+    );
+  }
+
+  return cache[id] = v;
+}
+
+console.log(gerry(voters, W, H));
