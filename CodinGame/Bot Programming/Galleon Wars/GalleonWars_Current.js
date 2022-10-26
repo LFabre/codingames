98297faf18a3f427399636d2f3f5ad31c5955578
LFabre <lucas.p.fabre@gmail.com>
2022-10-26 01:06:15 +0100

    feat(GalleonWars): Manual Navigation

--- CodinGame/Bot Programming/Galleon Wars/GalleonWars_Current.js
+++ CodinGame/Bot Programming/Galleon Wars/GalleonWars_Current.js
@@ -2,7 +2,7 @@ const GRID_HEIGHT = 21;
 const GRID_WIDTH = 23;
 
 const MINE_COOLDOWN = 4;
-const FIRE_COOLDOWN = 3;
+const FIRE_COOLDOWN = 2;
 
 const CANNON_RANGE = 13;
 
@@ -11,23 +11,36 @@ const COMMAND = {
   MOVE: 'MOVE',
   SLOWER: 'SLOWER',
   FASTER: 'FASTER',
-  LEFT: 'PORT',
-  RIGHT: 'STARBOARD',
+  PORT: 'PORT',
+  STARBOARD: 'STARBOARD',
   MINE: 'MINE',
   FIRE: 'FIRE',
-}
+};
 
 const ENTITY_TYPE = {
   MINE: 'MINE',
   SHIP: 'SHIP',
   BARREL: 'BARREL',
   CANNONBALL: 'CANNONBALL',
-}
+};
 
 const SHIP_TYPE = {
   ALLY: 1,
   ENEMY: 0
-}
+};
+
+const TURN_MAP = {
+  '1': COMMAND.STARBOARD,
+  '2': COMMAND.STARBOARD,
+  '3': COMMAND.STARBOARD,
+  '4': COMMAND.PORT,
+  '5': COMMAND.PORT,
+  '-1': COMMAND.PORT,
+  '-2': COMMAND.PORT,
+  '-3': COMMAND.PORT,
+  '-4': COMMAND.STARBOARD,
+  '-5': COMMAND.STARBOARD,
+};
 
 //:: Helpers
 function isShip(entityType) {
@@ -79,6 +92,14 @@ function debug(...a) {
 }
 
 //:: Coordinates
+function isValidCoord(y, x) {
+  return y >= 0 && y < GRID_HEIGHT && x >= 0 && x < GRID_WIDTH;
+}
+
+function isOnEdge(y, x) {
+  return y === 0 || y === GRID_HEIGHT - 1 || x === 0 || x === GRID_WIDTH - 1;
+}
+
 function offsetToCube(y, x) {
   let cubeX = x - (y - (y & 1) / 2);
   let cubeZ = y;
@@ -103,11 +124,7 @@ function distOffset(y, x, y1, x1) {
 }
 
 function dist(y, x, z, y1, x1, z1) {
-  return Math.sqrt(
-    Math.pow(x - x1, 2)
-    + Math.pow(y - y1, 2)
-    + Math.pow(z - z1, 2)
-  );
+  return Math.max(Math.abs(y - y1), Math.abs(x - x1), Math.abs(z - z1));
 }
 
 function nextOnDirection(y, x, d, speed) {
@@ -132,18 +149,50 @@ function nextOnDirection(y, x, d, speed) {
   }
 }
 
-function getNeighbors(aStarGrid, y, x) {
-  let coords;
+const COORDS_DIRECTIONS = {
+  // 4, 4
+  '0': {
+    '-1': { '0': 5, '1': 4 },
+    '0': { '1': 3, '-1': 0 },
+    '1': { '1': 2, '0': 1 },
+  },
+  // 3, 3
+  '1': {
+    '-1': { '0': 4, '-1': 5 },
+    '0': { '1': 3, '-1': 0 },
+    '1': { '0': 2, '-1': 1 },
+  }
+}
 
-  if (y % 2 === 0) {
-    coords = [[y, x + 1], [y - 1, x], [y - 1, x - 1], [y, x - 1], [y + 1, x - 1], [y + 1, x]]
-  } else {
-    coords = [[y, x + 1], [y - 1, x + 1], [y - 1, x], [y, x - 1], [y + 1, x], [y + 1, x + 1]]
+function getCoordsDirection(y, x, y1, x1) {
+  const isOdd = y & 1;
+  const dy = clamp(y - y1, -1, 1);
+  const dx = clamp(x - x1, -1, 1);
+
+  if (COORDS_DIRECTIONS[isOdd] === undefined
+    || COORDS_DIRECTIONS[isOdd][dy] === undefined
+    || COORDS_DIRECTIONS[isOdd][dy][dx] === undefined) {
+    throw new Error(`Invalid Direction ${y} ${x} ${y1} ${x1} - ${dy} ${dx}`);
   }
 
-  return coords
-    .filter(([y, x]) => y >= 0 && y < GRID_HEIGHT && x >= 0 && x < GRID_WIDTH)
-    .map(([y, x]) => aStarGrid[y][x])
+  return COORDS_DIRECTIONS[isOdd][dy][dx];
+}
+
+
+function getNeighborsCoords(y, x) {
+  if (y % 2 === 0) {
+    return [[y, x + 1], [y - 1, x], [y - 1, x - 1], [y, x - 1], [y + 1, x - 1], [y + 1, x]]
+  } else {
+    return [[y, x + 1], [y - 1, x + 1], [y - 1, x], [y, x - 1], [y + 1, x], [y + 1, x + 1]]
+  }
+}
+
+function nextShipCoordsOnDirection(y, x, d, speed = 1) {
+  return [
+    nextOnDirection(y, x, d, speed),
+    nextOnDirection(y, x, d + 1 > 5 ? 0 : d + 1, speed),
+    nextOnDirection(y, x, d ? d - 1 : 5, speed),
+  ];
 }
 
 function heuristic(aStarGrid, endX, endY) {
@@ -163,6 +212,7 @@ class PathFinder {
         row.push({
           y: y,
           x: x,
+          d: null,
           value: this.grid[y][x],
           H: null,
           G: Infinity,
@@ -179,10 +229,23 @@ class PathFinder {
     return aStarGrid;
   }
 
-  findPath(startY, startX, endY, endX) {
+  getNeighbors(aStarGrid, node) {
+    const coords = nextShipCoordsOnDirection(node.y, node.x, node.d);
+    const [y, x] = nextOnDirection(coords[0][0], coords[0][1], node.d);
+
+    if (!isValidCoord(y, x) || (aStarGrid[y][x].value && isMine(aStarGrid[y][x].value.type))) {
+      coords.shift();
+    }
+
+    return coords.filter(([y, x]) => isValidCoord(y, x))
+      .map(([y, x]) => aStarGrid[y][x])
+  }
+
+  findPath(startY, startX, startD, endY, endX) {
     const aStarGrid = this.initAStarGrid();
     const openList = [];
 
+    aStarGrid[startY][startX].d = startD;
     openList.push(aStarGrid[startY][startX]);
 
     while (openList.length) {
@@ -199,7 +262,7 @@ class PathFinder {
       if (currentNode.y === endY && currentNode.x === endX) {
         let curr = currentNode, path = [];
 
-        while (curr) {
+        while (curr.parent) {
           path.unshift(curr);
           curr = curr.parent;
         }
@@ -208,8 +271,12 @@ class PathFinder {
 
       currentNode.closed = true;
 
-      for (const neighbor of getNeighbors(aStarGrid, currentNode.y, currentNode.x)) {
-        if (neighbor.closed || (neighbor.value && (neighbor.value.type === ENTITY_TYPE.MINE || neighbor.value.type === ENTITY_TYPE.CANNONBALL))) {
+      for (const neighbor of this.getNeighbors(aStarGrid, currentNode)) {
+        const { closed, value } = neighbor;
+
+        const ignore = [ENTITY_TYPE.MINE, ENTITY_TYPE.CANNONBALL]
+        if (closed || value && ignore.includes(value.type)
+        ) {
           continue;
         }
 
@@ -226,6 +293,7 @@ class PathFinder {
 
         if (!beenVisited || gScore < neighbor.G) {
           neighbor.parent = currentNode;
+          neighbor.d = getCoordsDirection(currentNode.y, currentNode.x, neighbor.y, neighbor.x);
           neighbor.G = gScore;
           neighbor.F = neighbor.G + neighbor.H;
         }
@@ -365,17 +433,32 @@ class AllyShip extends Ship {
   slower() {
     console.log(COMMAND.SLOWER);
   }
+
+  turn(d) {
+    TURN_MAP[this.d - d] === COMMAND.PORT ? this.port() : this.starboard();
+  }
+
+  port() {
+    console.log(COMMAND.PORT);
+  }
+
+  starboard() {
+    console.log(COMMAND.STARBOARD);
+  }
 }
 
 class Ocean {
   constructor() {
     this.grid = [];
+
+    for (let r = 0; r < GRID_HEIGHT; r++) {
+      this.grid.push(new Array(GRID_WIDTH).fill(null));
+    }
   }
 
   resetGrid() {
-    this.grid = [];
-    for (let r = 0; r < GRID_HEIGHT; r++) {
-      this.grid.push(Array(GRID_WIDTH).fill(null));
+    for (let r = 0; r < this.grid.length; r++) {
+      this.grid[r].fill(null);
     }
   }
 
@@ -468,10 +551,43 @@ while (true) {
   //:: Ships Commands
   for (const aShip of allyShips) {
     const target = aShip.getTarget();
-
     const extraDistance = target.speed ? 1 : 0;
     const [enemyNextY, enemyNextX] = target.futureCoordinate(extraDistance + 2 * target.speed);
     const [enemyPrevY, enemyPrevX] = target.futureCoordinate(Math.min(-1, -1 * target.speed));
+
+    let moveToY, moveToX;
+    let by, bx;
+
+    if (barrels.length) {
+      let d = Number.MAX_VALUE
+      for (const b of barrels) {
+        let _d = distOffset(aShip.y, aShip.x, b.y, b.x);
+        if (_d < d) {
+          bx = b.x;
+          by = b.y;
+          d = _d;
+        }
+      }
+    }
+
+    // Set where to go
+    if (aShip.rum < 200) {
+      moveToY = by;
+      moveToX = bx;
+      debug('Move to Barrel')
+    } else {
+      const m = target.futureCoordinate(3)
+      moveToY = clampHeight(m[0], 1);
+      moveToX = clampWidth(m[1], 1);
+      debug('Move to Enemy')
+    }
+
+    debug({ moveToY, moveToX });
+
+    // Create Path
+    finder = new PathFinder(ocean.grid);
+    const path = finder.findPath(aShip.y, aShip.x, aShip.d, moveToY, moveToX);
+    ocean.setPath(path);
 
     if (target
       && aShip.isInFireRange(enemyNextY, enemyNextX)
@@ -479,39 +595,38 @@ while (true) {
       && (aShip.speed || !target.speed)
     ) {
       aShip.fire(enemyNextY, enemyNextX);
+    } else if (path.length) {
 
-    } else {
-      const m = target.futureCoordinate(4)
-      let moveToY = m[0], moveToX = m[1];
-
-      if (barrels.length && aShip.rum < 80) {
-        let d = Number.MAX_VALUE
-        for (const b of barrels) {
-          let _d = distOffset(aShip.y, aShip.x, b.y, b.x);
-
-          if (_d < d) {
-            moveToX = b.x;
-            moveToY = b.y;
-            d = _d;
-          }
+      let turnIdx = -1;
+      for (let i = 0; i <= aShip.speed + 1 && i < path.length; i++) {
+        if (path[i].d !== aShip.d) {
+          turnIdx = i;
         }
       }
 
-      if (!aShip.hasMoved()) {
-        const myNexts = nextOnDirection(aShip.y, aShip.x, aShip.d, 2);
-        moveToY = myNexts[0];
-        moveToX = myNexts[1];
+      debug({ d: aShip.d, turnIdx })
+
+      if (
+        turnIdx === 0
+        || turnIdx > 0 && aShip.speed
+        || turnIdx >= 0 && isOnEdge(aShip.y, aShip.x) && !aShip.speed
+      ) {
+        aShip.turn(path[turnIdx].d);
+      } else {
+
+        if (!aShip.speed) {
+          aShip.faster();
+        } else {
+          aShip.mine();
+        }
+
       }
-      
-      moveToY = clampHeight(moveToY, 1);
-      moveToX = clampWidth(moveToX, 1);
+    } else {
+      debug(' NO PATH! ')
+      aShip.move(10, 10);
 
-      finder = new PathFinder(ocean.grid);
-      const path = finder.findPath(aShip.y, aShip.x, moveToY, moveToX);
-      ocean.setPath(path);
-      ocean.printOffset();
-
-      aShip.move(moveToY, moveToX);
     }
   }
+
+  ocean.printOffset();
 }
